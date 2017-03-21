c2f712e5a76f96364622a11f36d6c413bd4ce615
Tarun Prajapati <tarunprajapati@taruns-macbook-air.local>
2017-03-21 21:33:10 +0530

    Initial Commit

---
+++ Movie Finder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movie Finder
+//
+//  Created by Tarun Prajapati on 21/03/17.
+//  Copyright © 2017 Tarun Prajapati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
